ad959891f08d84577c49d21315e3b159c7c9116f
Norma Urcadiz <norma.urcadiz.p@gmail.com>
2020-09-11 17:24:52 -0600

    Norma PWA1

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,22 @@
-
 var url = window.location.href;
-var swLocation = '/twittor/sw.js';
+var swLocation = '/PWA/sw.js';
 
 var swReg;
 
-if ( navigator.serviceWorker ) {
+if (navigator.serviceWorker) {
 
 
-    if ( url.includes('localhost') ) {
+    if (url.includes('localhost')) {
         swLocation = '/sw.js';
     }
 
 
     window.addEventListener('load', function() {
 
-        navigator.serviceWorker.register( swLocation ).then( function(reg){
+        navigator.serviceWorker.register(swLocation).then(function(reg) {
 
             swReg = reg;
-            swReg.pushManager.getSubscription().then( verificaSuscripcion );
+            swReg.pushManager.getSubscription().then(verificaSuscripcion);
 
         });
 
@@ -44,41 +43,41 @@ var googleMapKey = 'AIzaSyA5mjCwx1TRLuBAjwQw84WE6h5ErSe7Uj8';
 // AIzaSyD4YFaT5DvwhhhqMpDP2pBInoG8BTzA9JY
 // AIzaSyAbPC1F9pWeD70Ny8PHcjguPffSLhT-YF8
 
-var titulo      = $('#titulo');
-var nuevoBtn    = $('#nuevo-btn');
-var salirBtn    = $('#salir-btn');
+var titulo = $('#titulo');
+var nuevoBtn = $('#nuevo-btn');
+var salirBtn = $('#salir-btn');
 var cancelarBtn = $('#cancel-btn');
-var postBtn     = $('#post-btn');
-var avatarSel   = $('#seleccion');
-var timeline    = $('#timeline');
+var postBtn = $('#post-btn');
+var avatarSel = $('#seleccion');
+var timeline = $('#timeline');
 
-var modal       = $('#modal');
+var modal = $('#modal');
 var modalAvatar = $('#modal-avatar');
-var avatarBtns  = $('.seleccion-avatar');
-var txtMensaje  = $('#txtMensaje');
+var avatarBtns = $('.seleccion-avatar');
+var txtMensaje = $('#txtMensaje');
 
-var btnActivadas    = $('.btn-noti-activadas');
+var btnActivadas = $('.btn-noti-activadas');
 var btnDesactivadas = $('.btn-noti-desactivadas');
 
 
-var btnLocation      = $('#location-btn');
+var btnLocation = $('#location-btn');
 
-var modalMapa        = $('.modal-mapa');
+var modalMapa = $('.modal-mapa');
 
-var btnTomarFoto     = $('#tomar-foto-btn');
-var btnPhoto         = $('#photo-btn');
+var btnTomarFoto = $('#tomar-foto-btn');
+var btnPhoto = $('#photo-btn');
 var contenedorCamara = $('.camara-contenedor');
 
-var lat  = null;
-var lng  = null; 
-var foto = null; 
+var lat = null;
+var lng = null;
+var foto = null;
 
 // El usuario, contiene el ID del héroe seleccionado
 var usuario;
 
 // Init de la camara class
 // document.getElementById('player');
-const camara = new Camara( $('#player')[0] );
+const camara = new Camara($('#player')[0]);
 
 
 
@@ -88,7 +87,7 @@ function crearMensajeHTML(mensaje, personaje, lat, lng, foto) {
 
     // console.log(mensaje, personaje, lat, lng);
 
-    var content =`
+    var content = `
     <li class="animated fadeIn fast"
         data-user="${ personaje }"
         data-mensaje="${ mensaje }"
@@ -104,27 +103,27 @@ function crearMensajeHTML(mensaje, personaje, lat, lng, foto) {
                 <br/>
                 ${ mensaje }
                 `;
-    
-    if ( foto ) {
+
+    if (foto) {
         content += `
                 <br>
                 <img class="foto-mensaje" src="${ foto }">
         `;
     }
-        
+
     content += `</div>        
                 <div class="arrow"></div>
             </div>
         </li>
     `;
 
-    
+
     // si existe la latitud y longitud, 
     // llamamos la funcion para crear el mapa
-    if ( lat ) {
-        crearMensajeMapa( lat, lng, personaje );
+    if (lat) {
+        crearMensajeMapa(lat, lng, personaje);
     }
-    
+
     // Borramos la latitud y longitud por si las usó
     lat = null;
     lng = null;
@@ -170,9 +169,9 @@ function crearMensajeMapa(lat, lng, personaje) {
 
 
 // Globals
-function logIn( ingreso ) {
+function logIn(ingreso) {
 
-    if ( ingreso ) {
+    if (ingreso) {
         nuevoBtn.removeClass('oculto');
         salirBtn.removeClass('oculto');
         timeline.removeClass('oculto');
@@ -185,7 +184,7 @@ function logIn( ingreso ) {
         avatarSel.removeClass('oculto');
 
         titulo.text('Seleccione Personaje');
-    
+
     }
 
 }
@@ -213,10 +212,10 @@ salirBtn.on('click', function() {
 nuevoBtn.on('click', function() {
 
     modal.removeClass('oculto');
-    modal.animate({ 
+    modal.animate({
         marginTop: '-=1000px',
         opacity: 1
-    }, 200 );
+    }, 200);
 
 });
 
@@ -224,15 +223,15 @@ nuevoBtn.on('click', function() {
 // Boton de cancelar mensaje
 cancelarBtn.on('click', function() {
 
-    if ( !modal.hasClass('oculto') ) {
-        modal.animate({ 
+    if (!modal.hasClass('oculto')) {
+        modal.animate({
             marginTop: '+=1000px',
             opacity: 0
-         }, 200, function() {
-             modal.addClass('oculto');
-             modalMapa.addClass('oculto');
-             txtMensaje.val('');
-         });
+        }, 200, function() {
+            modal.addClass('oculto');
+            modalMapa.addClass('oculto');
+            txtMensaje.val('');
+        });
     }
 
 });
@@ -241,7 +240,7 @@ cancelarBtn.on('click', function() {
 postBtn.on('click', function() {
 
     var mensaje = txtMensaje.val();
-    if ( mensaje.length === 0 ) {
+    if (mensaje.length === 0) {
         cancelarBtn.click();
         return;
     }
@@ -256,19 +255,19 @@ postBtn.on('click', function() {
 
 
     fetch('api', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify( data )
-    })
-    .then( res => res.json() )
-    .then( res => console.log( 'app.js', res ))
-    .catch( err => console.log( 'app.js error:', err ));
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(res => console.log('app.js', res))
+        .catch(err => console.log('app.js error:', err));
 
 
-    crearMensajeHTML( mensaje, usuario, lat, lng, foto );
-    
+    crearMensajeHTML(mensaje, usuario, lat, lng, foto);
+
     foto = null;
 });
 
@@ -278,13 +277,13 @@ postBtn.on('click', function() {
 function getMensajes() {
 
     fetch('api')
-        .then( res => res.json() )
-        .then( posts => {
+        .then(res => res.json())
+        .then(posts => {
 
             console.log(posts);
 
-            posts.forEach( post => 
-                crearMensajeHTML( post.mensaje, post.user, post.lat, post.lng, post.foto ));
+            posts.forEach(post =>
+                crearMensajeHTML(post.mensaje, post.user, post.lat, post.lng, post.foto));
         });
 
 
@@ -297,7 +296,7 @@ getMensajes();
 // Detectar cambios de conexión
 function isOnline() {
 
-    if ( navigator.onLine ) {
+    if (navigator.onLine) {
         // tenemos conexión
         // console.log('online');
         $.mdtoast('Online', {
@@ -307,7 +306,7 @@ function isOnline() {
         });
 
 
-    } else{
+    } else {
         // No tenemos conexión
         $.mdtoast('Offline', {
             interaction: true,
@@ -318,17 +317,17 @@ function isOnline() {
 
 }
 
-window.addEventListener('online', isOnline );
-window.addEventListener('offline', isOnline );
+window.addEventListener('online', isOnline);
+window.addEventListener('offline', isOnline);
 
 isOnline();
 
 
 // Notificaciones
-function verificaSuscripcion( activadas ) {
+function verificaSuscripcion(activadas) {
 
-    if ( activadas ) {
-        
+    if (activadas) {
+
         btnActivadas.removeClass('oculto');
         btnDesactivadas.addClass('oculto');
 
@@ -360,23 +359,23 @@ function enviarNotificacion() {
 
 function notificarme() {
 
-    if ( !window.Notification ) {
+    if (!window.Notification) {
         console.log('Este navegador no soporta notificaciones');
         return;
     }
 
-    if ( Notification.permission === 'granted' ) {
-        
+    if (Notification.permission === 'granted') {
+
         // new Notification('Hola Mundo! - granted');
         enviarNotificacion();
 
-    } else if ( Notification.permission !== 'denied' || Notification.permission === 'default' )  {
+    } else if (Notification.permission !== 'denied' || Notification.permission === 'default') {
 
-        Notification.requestPermission( function( permission ) {
+        Notification.requestPermission(function(permission) {
 
-            console.log( permission );
+            console.log(permission);
 
-            if ( permission === 'granted' ) {
+            if (permission === 'granted') {
                 // new Notification('Hola Mundo! - pregunta');
                 enviarNotificacion();
             }
@@ -400,38 +399,38 @@ function getPublicKey() {
     //     .then( console.log );
 
     return fetch('api/key')
-        .then( res => res.arrayBuffer())
+        .then(res => res.arrayBuffer())
         // returnar arreglo, pero como un Uint8array
-        .then( key => new Uint8Array(key) );
+        .then(key => new Uint8Array(key));
 
 
 }
 
 // getPublicKey().then( console.log );
-btnDesactivadas.on( 'click', function() {
+btnDesactivadas.on('click', function() {
 
-    if ( !swReg ) return console.log('No hay registro de SW');
+    if (!swReg) return console.log('No hay registro de SW');
 
-    getPublicKey().then( function( key ) {
+    getPublicKey().then(function(key) {
 
         swReg.pushManager.subscribe({
-            userVisibleOnly: true,
-            applicationServerKey: key
-        })
-        .then( res => res.toJSON() )
-        .then( suscripcion => {
-
-            // console.log(suscripcion);
-            fetch('api/subscribe', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify( suscripcion )
+                userVisibleOnly: true,
+                applicationServerKey: key
             })
-            .then( verificaSuscripcion )
-            .catch( cancelarSuscripcion );
+            .then(res => res.toJSON())
+            .then(suscripcion => {
+
+                // console.log(suscripcion);
+                fetch('api/subscribe', {
+                        method: 'POST',
+                        headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify(suscripcion)
+                    })
+                    .then(verificaSuscripcion)
+                    .catch(cancelarSuscripcion);
 
 
-        });
+            });
 
 
     });
@@ -443,16 +442,16 @@ btnDesactivadas.on( 'click', function() {
 
 function cancelarSuscripcion() {
 
-    swReg.pushManager.getSubscription().then( subs => {
+    swReg.pushManager.getSubscription().then(subs => {
 
-        subs.unsubscribe().then( () =>  verificaSuscripcion(false) );
+        subs.unsubscribe().then(() => verificaSuscripcion(false));
 
     });
 
 
 }
 
-btnActivadas.on( 'click', function() {
+btnActivadas.on('click', function() {
 
     cancelarSuscripcion();
 
@@ -464,7 +463,7 @@ btnActivadas.on( 'click', function() {
 function mostrarMapaModal(lat, lng) {
 
     $('.modal-mapa').remove();
-    
+
     var content = `
             <div class="modal-mapa">
                 <iframe
@@ -476,7 +475,7 @@ function mostrarMapaModal(lat, lng) {
             </div>
     `;
 
-    modal.append( content );
+    modal.append(content);
 }
 
 
@@ -494,10 +493,10 @@ btnLocation.on('click', () => {
     });
 
 
-    navigator.geolocation.getCurrentPosition( pos => {
+    navigator.geolocation.getCurrentPosition(pos => {
 
-        console.log( pos );
-        mostrarMapaModal( pos.coords.latitude, pos.coords.longitude );
+        console.log(pos);
+        mostrarMapaModal(pos.coords.latitude, pos.coords.longitude);
 
         lat = pos.coords.latitude;
         lng = pos.coords.longitude;
@@ -529,7 +528,7 @@ btnTomarFoto.on('click', () => {
     foto = camara.tomarFoto();
 
     camara.apagar();
-    
+
     // console.log(foto);
 
 });
@@ -546,14 +545,14 @@ btnTomarFoto.on('click', () => {
 timeline.on('click', 'li', function() {
 
     // console.log(  $(this)  );
-    
-    let tipo    = $(this).data('tipo');
-    let lat     = $(this).data('lat');
-    let lng     = $(this).data('lng');
+
+    let tipo = $(this).data('tipo');
+    let lat = $(this).data('lat');
+    let lng = $(this).data('lng');
     let mensaje = $(this).data('mensaje');
-    let user    = $(this).data('user');
-    
-    console.log({ tipo, lat, lng, mensaje, user  });
+    let user = $(this).data('user');
+
+    console.log({ tipo, lat, lng, mensaje, user });
 
 
     const shareOpts = {
@@ -561,17 +560,13 @@ timeline.on('click', 'li', function() {
         text: mensaje
     };
 
-    if ( tipo === 'mapa' ) {
+    if (tipo === 'mapa') {
         shareOpts.text = 'Mapa';
-        shareOpts.url  = `https://www.google.com/maps/@${ lat },${ lng },15z`;
+        shareOpts.url = `https://www.google.com/maps/@${ lat },${ lng },15z`;
     }
 
     navigator.share(shareOpts)
-      .then(() => console.log('Successful share'))
-      .catch((error) => console.log('Error sharing', error));
+        .then(() => console.log('Successful share'))
+        .catch((error) => console.log('Error sharing', error));
 
 });
-
-
-
-
